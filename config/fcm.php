c0336a3489cd17e0bdbcfd4dc5b7f04845e82c7c
Almadih <modialmadih@gmail.com>
2025-05-31 10:47:06 +0200

    Remove geocoder config, update FCM config, and modify auth and test files
    - Delete unused geocoder.php config file to clean up project configuration
    - Modify auth routes and enhance authentication-related feature tests for robustness

--- config/fcm.php
+++ config/fcm.php
@@ -9,9 +9,9 @@ return [
             'service_account' => env('GOOGLE_SERVICE_ACCOUNT'),
         ],
 
-//        'fcm.project-1' => [
-//            'project' => env('PROJECT_1_GOOGLE_PROJECT'),
-//            'service_account' => env('PROJECT_1_SERVICE_ACCOUNT'),
-//        ],
+        //        'fcm.project-1' => [
+        //            'project' => env('PROJECT_1_GOOGLE_PROJECT'),
+        //            'service_account' => env('PROJECT_1_SERVICE_ACCOUNT'),
+        //        ],
     ],
 ];
