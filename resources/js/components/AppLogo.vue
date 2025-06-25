b5b2ca1d7d6c1426083d135e293703199ca16fd9
Almadih <modialmadih@gmail.com>
2025-06-25 16:57:55 +0200

    Improves app logo responsiveness
    Dynamically adjusts the app logo size based on the sidebar state, enhancing the user experience by providing a more responsive and visually appealing interface.

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSidebar } from './ui/sidebar';
+
+
+const { open: isOpen } = useSidebar()
+
+</script>
+
 
 <template>
     <div class="flex items-center space-x-2">
-        <img src="/logo.webp" class="h-11 w-auto" alt="infra report logo" />
+        <img src="/logo.webp" :class="{ 'h-11 w-auto': isOpen, 'h-auto w-24': !isOpen }" alt="infra report logo" />
     </div>
 </template>
