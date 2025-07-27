ebd1c8aa4c94c033d8485718c2434555b883b413
Almadih <modialmadih@gmail.com>
2025-07-27 15:46:50 +0200

    remove unused log

--- app/Http/Controllers/Users/UpdateProfileController.php
+++ app/Http/Controllers/Users/UpdateProfileController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class UpdateProfileController extends Controller
 {
@@ -15,8 +14,6 @@ class UpdateProfileController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'show_info_to_public' => ['required', 'boolean'],
         ]);
-
-        Log::debug(json_encode($validated));
 
         Auth::user()->update($validated);
 
